44066082927065399c7884e8d629a0f10a7c3176
robloxexecutorth <ppmuhaha8@gmail.com>
2026-04-08 19:32:40 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from threading import Thread
+import os # เพิ่มอันนี้เข้ามา
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive!"
+    return "RETH Guard is awake and monitoring!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # เปลี่ยนจาก 8080 เป็นการดึงค่าจาก Environment Variable ของ Render
+    port = int(os.environ.get("PORT", 8080)) 
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
